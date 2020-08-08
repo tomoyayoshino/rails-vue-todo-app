6d4525e3afb6ca5e8cce93709f185d36ce924961
tomoyayoshino <t.yoshino0331@gmail.com>
2020-08-08 16:54:41 +0900

    コンポーネントを作成し、テーブルをリファクタリング

--- app/javascript/to_dos/index.vue
+++ app/javascript/to_dos/index.vue
@@ -1,70 +1,26 @@
 <template>
   <div id="app">
-    <el-tabs v-model="toDo">
+    <el-tabs v-model="activeName">
       <el-tab-pane label="ToDo" name="toDo">
-        <el-table
-          :data="filter(toDos, false)"
-          style="width: 100%">
-          <el-table-column
-            prop="finished">
-            <template v-slot="scope">
-              <el-checkbox
-                v-model="scope.row.finished"
-                @change="updateToDo(scope.row.id, scope.row.finished)"></el-checkbox>
-            </template>
-          </el-table-column>
-          <el-table-column
-            prop="title">
-          </el-table-column>
-          <el-table-column
-            prop="expired_at">
-          </el-table-column>
-          <el-table-column
-            width="120">
-            <template v-slot="scope">
-          　　<el-button
-              　　@click="destroyToDo(scope.row.id)"
-                type="danger"
-                icon="el-icon-delete"
-                circle></el-button>
-        　　</template>
-          </el-table-column>
-        </el-table>
+        <to-do-table
+          :to-dos="filter(toDos, false)"
+          @update="updateToDo"
+          @destroy="destroyToDo">
+        </to-do-table>
       </el-tab-pane>
       <el-tab-pane label="Completed" name="finishedToDo">
-        <el-table
-          :data="filter(toDos, true)"
-          style="width: 100%">
-          <el-table-column
-            prop="finished">
-            <template v-slot="scope">
-              <el-checkbox
-                v-model="scope.row.finished"
-                @change="updateToDo(scope.row.id, scope.row.finished)"></el-checkbox>
-            </template>
-          </el-table-column>
-          <el-table-column
-            prop="title">
-          </el-table-column>
-          <el-table-column
-            prop="expired_at">
-          </el-table-column>
-          <el-table-column
-            width="120">
-            <template v-slot="scope">
-          　　<el-button
-              　　@click="destroyToDo(scope.row.id)"
-                type="danger"
-                icon="el-icon-delete"
-                circle></el-button>
-        　　</template>
-          </el-table-column>
-        </el-table>
+        <to-do-table
+          :to-dos="filter(toDos, true)"
+          @update="updateToDo"
+          @destroy="destroyToDo">
+        </to-do-table>
       </el-tab-pane>
     </el-tabs>
   </div>
 </template>
+
 <script>
+  import ToDoTable from '../to_dos/to-do-table'
   import axios from 'axios'
   import {reject, filter} from 'lodash';
   export default {
@@ -73,6 +29,9 @@
       toDos: [],
       activeName: 'toDo'
     }
+  },
+  components: {
+    ToDoTable
   },
   created() {
     axios.get('/api/v1/to_dos')
---
+++ app/javascript/to_dos/to-do-table.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-table
+    :data="toDos"
+    style="width: 100%">
+    <el-table-column
+      prop="finished">
+      <template v-slot="scope">
+        <el-checkbox
+          v-model="scope.row.finished"
+          @change="$emit('update', scope.row.id, scope.row.finished)"></el-checkbox>
+      </template>
+    </el-table-column>
+    <el-table-column
+      prop="title">
+    </el-table-column>
+    <el-table-column
+      prop="expired_at">
+    </el-table-column>
+    <el-table-column
+      width="120">
+      <template v-slot="scope">
+        <el-button
+          @click="$emit('destroy', scope.row.id)"
+          type="danger"
+          icon="el-icon-delete"
+          circle></el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+
+  export default {
+    props: ['toDos']
+  }
+
+</script>
