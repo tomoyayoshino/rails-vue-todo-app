b83f70bc35672124288dea5246e4a2657e43eee4
tomoyayoshino <t.yoshino0331@gmail.com>
2020-08-08 14:36:32 +0900

    new,createアクションの追加

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -2,4 +2,23 @@ class ToDosController < ApplicationController
   def index
     @to_dos = ToDo.all
   end
+
+  def new
+    @to_do = ToDo.new
+  end
+
+  def create
+    @to_do = ToDo.new(to_do_params)
+    if @to_do.save
+      redirect_to to_dos_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def to_do_params
+    params.require(:to_do).permit(:title)
+  end
 end
