553918fdb427861d7f08e76b8ba23212d4ae90b5
tomoyayoshino <t.yoshino0331@gmail.com>
2020-08-08 16:30:55 +0900

    チェックボックスの追加

--- app/controllers/api/v1/to_dos_controller.rb
+++ app/controllers/api/v1/to_dos_controller.rb
@@ -3,10 +3,23 @@ class Api::V1::ToDosController < ActionController::API
     @to_dos = ToDo.all
   end
 
+  def update
+    @to_do = ToDo.find(params[:id])
+    if @to_do.update(to_do_params)
+      render status: 200, json: { status: 200 }
+    end
+  end
+
   def destroy
     to_do = ToDo.find(params[:id])
     if to_do.destroy
       render status: 200, json: { status: 200 }
     end
+  end
+
+  private
+
+  def to_do_params
+    params.require(:to_do).permit(:finished)
   end
 end
--- app/javascript/to_dos/index.vue
+++ app/javascript/to_dos/index.vue
@@ -1,25 +1,34 @@
 <template>
-  <el-table
-    :data="toDos"
-    style="width: 100%">
-    <el-table-column
-      prop="title">
-    </el-table-column>
-    <el-table-column
-      prop="expired_at">
-    </el-table-column>
-    <el-table-column
-      width="120">
-      <template v-slot="scope">
-    　　<el-button
-        　　@click="destroyToDo(scope.row.id)"
-          type="danger"
-          icon="el-icon-delete"
-          circle></el-button>
-  　　</template>
-    </el-table-column>
-
-  </el-table>
+  <div id="app">
+    <el-table
+      :data="toDos"
+      style="width: 100%">
+      <el-table-column
+        prop="finished">
+        <template v-slot="scope">
+          <el-checkbox
+            v-model="scope.row.finished"
+            @change="updateToDo(scope.row.id, scope.row.finished)"></el-checkbox>
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="title">
+      </el-table-column>
+      <el-table-column
+        prop="expired_at">
+      </el-table-column>
+      <el-table-column
+        width="120">
+        <template v-slot="scope">
+      　　<el-button
+          　　@click="destroyToDo(scope.row.id)"
+            type="danger"
+            icon="el-icon-delete"
+            circle></el-button>
+    　　</template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 <script>
   import axios from 'axios'
@@ -44,6 +53,14 @@
             this.toDos = reject(this.toDos, ['id', id]);
           }
         });
+    },
+    updateToDo(id, finished) {
+      axios.patch('/api/v1/to_dos/' + id, {to_do: {finished: finished}})
+        .then(res => {
+          if (res.status === 200) {
+            console.log(res)
+          }
+      })
     }
   }
 }
