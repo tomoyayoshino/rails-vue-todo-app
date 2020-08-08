352cf83e55649f583825dcb090dcd1e61c02795d
tomoyayoshino <t.yoshino0331@gmail.com>
2020-08-08 14:25:40 +0900

    todoモデル、コントローラーの作成

---
+++ db/migrate/20200808052055_create_to_dos.rb
@@ -0,0 +1,9 @@
+class CreateToDos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :to_dos do |t|
+      t.string :title, null: false
+      t.boolean :finished, default: false, null: false
+      t.timestamps
+    end
+  end
+end
